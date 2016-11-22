f9aeddee81fcd141d3d88cf82c62ef1c7133d678
John Doherty <doherty.j@cambridgeassessment.org.uk>
2016-11-22 15:08:34 +0000

    updated google-search-steps to include comments and updated related readme section"

--- step-definitions/google-search-steps.js
+++ step-definitions/google-search-steps.js
@@ -2,18 +2,25 @@ module.exports = function () {
 
     this.When(/^I search Google for "([^"]*)"$/, function (searchQuery) {
 
-        return driver.get('http://www.google.com').then(function(){
+        return helpers.loadPage('http://www.google.com').then(function() {
+
+            // use a method on the page object which also returns a promise
             return page.googleSearch.preformSearch(searchQuery);
         })
     });
 
     this.Then(/^I should see some results$/, function () {
 
-        return driver.wait(until.elementsLocated(by.css('div.g')), 10000).then(function(){
+        // driver wair returns a promise so return that
+        return driver.wait(until.elementsLocated(by.css('div.g')), 10000).then(function() {
+
+            // return the promise of an element to the following then.
             return driver.findElements(by.css('div.g'));
         })
         .then(function (elements) {
+
+            // verify this element has children
             expect(elements.length).to.not.equal(0);
-        });        
+        });
     });
 };
