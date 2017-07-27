a4f0e8327040e1cc134ab35bf36879d318c98fd6
Lee Eggebroten <lee@tripping.com>
2017-07-27 12:53:35 -0700

    Do not force browser to full screen. Allow the driver generators to set the desired state.

--- runtime/firefoxDriver.js
+++ runtime/firefoxDriver.js
@@ -1,13 +1,18 @@
 "use strict";
 (function(){
     var firefox = require('geckodriver');
+    var selenium = require('selenium-webdriver');
 
     module.exports = function () {
-        return new selenium.Builder().withCapabilities({
+        var driver = new selenium.Builder().withCapabilities({
             browserName: 'firefox',
             javascriptEnabled: true,
             acceptSslCerts: true,
             'webdriver.firefox.bin': firefox.path
         }).build();
+
+        driver.manage().window().maximize();
+
+        return driver;
     }
 })();
