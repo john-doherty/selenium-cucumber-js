294f142454b96d7148558a12ce8da8662f821b03
CSadek <christina.crossover@gmial.com>
2018-03-18 15:14:59 +0200

    revert

--- runtime/chromeDriver.js
+++ runtime/chromeDriver.js
@@ -1,16 +1,22 @@
 'use strict';
-
+var chromedriver = require('chromedriver');
 var selenium = require('selenium-webdriver');
 
 /**
  * Creates a Selenium WebDriver using Chrome as the browser
  * @returns {ThenableWebDriver} selenium web driver
  */
-module.exports = function () {
+module.exports = function() {
 
-    var driver = new selenium.Builder().
-        usingServer('http://localhost:4444/wd/hub').
-        withCapabilities(selenium.Capabilities.chrome()).build();
+    var driver = new selenium.Builder().withCapabilities({
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        chromeOptions: {
+            args: ['start-maximized']
+        },
+        path: chromedriver.path
+        }).build();
 
     driver.manage().window().maximize();
 
